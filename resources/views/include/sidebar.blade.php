936b847217f57f89a8de2668621998b1cccf48db
alissonalberini <alissonalberini94@gmail.com>
2018-10-03 17:30:57 -0400

    Removidos os itens do menu, que ainda não serão transcritos. e os existente, implementando a validação de url, e tradução.

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -12,8 +12,8 @@
                         <span class="fa fa-home"></span><span class="mtext">Home</span>
                     </a>
                     <ul class="submenu">
-                        <li><a href="/dashboard">Dashboard 1</a></li>
-                        <li><a href="/index2">Dashboard 2</a></li>
+                        <li><a href="/dashboard">{{ __('Dashboard') }} 1</a></li>
+                        <li><a href="/dashboard2">{{ __('Dashboard') }} 2</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
@@ -21,13 +21,7 @@
                         <span class="fa fa-pencil"></span><span class="mtext">Formulários</span>
                     </a>
                     <ul class="submenu">
-                        <li><a href="/form-basic">Formulário Basico</a></li>
-                        <li><a href="/advanced-components">Advanced Components</a></li>
-                        <li><a href="/form-wizard">Form Wizard</a></li>
-                        <li><a href="/html5-editor">HTML5 Editor</a></li>
-                        <li><a href="/form-pickers">Form Pickers</a></li>
-                        <li><a href="/image-cropper">Image Cropper</a></li>
-                        <li><a href="/image-dropzone">Image Dropzone</a></li>
+                        <li><a href="{{ route('users.create') }}">{{ __('New User') }}</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
@@ -35,8 +29,9 @@
                         <span class="fa fa-table"></span><span class="mtext">Tables</span>
                     </a>
                     <ul class="submenu">
-                        <li><a href="/basic-table">Basic Tables</a></li>
-                        <li><a href="/datatable">DataTables</a></li>
+                        <li><a href="{{ route('users.index') }}">{{ __('Users') }} Laravel</a></li>
+                        <li><a href="{{ route('users.index2') }}">{{ __('Users') }} Datatables</a></li>
+                        <li><a href="{{ route('users.index3') }}">{{ __('Users') }} Bootstrap-tables</a></li>
                     </ul>
                 </li>
                 <li>
@@ -46,108 +41,11 @@
                 </li>
                 <li class="dropdown">
                     <a href="javascript:;" class="dropdown-toggle">
-                        <span class="fa fa-desktop"></span><span class="mtext"> UI Elements </span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="/ui-buttons">Buttons</a></li>
-                        <li><a href="/ui-cards">Cards</a></li>
-                        <li><a href="/ui-cards-hover">Cards Hover</a></li>
-                        <li><a href="/ui-modals">Modals</a></li>
-                        <li><a href="/ui-tabs">Tabs</a></li>
-                        <li><a href="/ui-tooltip-popover">Tooltip &amp; Popover</a></li>
-                        <li><a href="/ui-sweet-alert">Sweet Alert</a></li>
-                        <li><a href="/ui-notification">Notification</a></li>
-                        <li><a href="/ui-timeline">Timeline</a></li>
-                        <li><a href="/ui-progressbar">Progressbar</a></li>
-                        <li><a href="/ui-typography">Typography</a></li>
-                        <li><a href="/ui-list-group">List group</a></li>
-                        <li><a href="/ui-range-slider">Range slider</a></li>
-                        <li><a href="/ui-carousel">Carousel</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="fa fa-paint-brush"></span><span class="mtext">Icons</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="/font-awesome">FontAwesome Icons</a></li>
-                        <li><a href="/foundation">Foundation Icons</a></li>
-                        <li><a href="/ionicons">Ionicons Icons</a></li>
-                        <li><a href="/themify">Themify Icons</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="fa fa-plug"></span><span class="mtext">Additional Pages</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="/video-player">Video Player</a></li>
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="/forgot-password">Forgot Password</a></li>
-                        <li><a href="/reset-password">Reset Password</a></li>
-                        <li><a href="/403">403</a></li>
-                        <li><a href="/404">404</a></li>
-                        <li><a href="/500">500</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="fa fa-pie-chart"></span><span class="mtext">Charts</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="/highchart">Highchart</a></li>
-                        <li><a href="/knob-chart">jQuery Knob</a></li>
-                        <li><a href="/jvectormap">jvectormap</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
                         <span class="fa fa-clone"></span><span class="mtext">Extra Pages</span>
                     </a>
                     <ul class="submenu">
                         <li><a href="/blank">Blank</a></li>
-                        <li><a href="/contact-directory">Contact Directory</a></li>
-                        <li><a href="/blog">Blog</a></li>
-                        <li><a href="/blog-detail">Blog Detail</a></li>
-                        <li><a href="/product">Product</a></li>
-                        <li><a href="/product-detail">Product Detail</a></li>
-                        <li><a href="/faq">FAQ</a></li>
-                        <li><a href="/profile">Profile</a></li>
-                        <li><a href="/gallery">Gallery</a></li>
-                        <li><a href="/pricing-table">Pricing Tables</a></li>
                     </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <span class="fa fa-list"></span><span class="mtext">Multi Level Menu</span>
-                    </a>
-                    <ul class="submenu">
-                        <li><a href="javascript:;">Level 1</a></li>
-                        <li><a href="javascript:;">Level 1</a></li>
-                        <li><a href="javascript:;">Level 1</a></li>
-                        <li class="dropdown">
-                            <a href="javascript:;" class="dropdown-toggle">
-                                <span class="fa fa-plug"></span><span class="mtext">Level 2</span>
-                            </a>
-                            <ul class="submenu child">
-                                <li><a href="javascript:;">Level 2</a></li>
-                                <li><a href="javascript:;">Level 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="javascript:;">Level 1</a></li>
-                        <li><a href="javascript:;">Level 1</a></li>
-                        <li><a href="javascript:;">Level 1</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="/sitemap" class="dropdown-toggle no-arrow">
-                        <span class="fa fa-sitemap"></span><span class="mtext">Sitemap</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/chat" class="dropdown-toggle no-arrow">
-                        <span class="fa fa-comments-o"></span><span class="mtext">Chat <span class="fi-burst-new text-danger new"></span></span>
-                    </a>
                 </li>
                 <li>
                     <a href="/invoice" class="dropdown-toggle no-arrow">
